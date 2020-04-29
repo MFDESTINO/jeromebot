04ef266ce08f3e7854a91d6813747f9727c5ce9a
couto0 <daniel.couto64@gmail.com>
2020-04-29 15:39:55 -0300

    ajustes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from PIL import Image, ImageFont, ImageDraw
 from requests_html import HTMLSession
 from random import choice
@@ -8,11 +10,10 @@ now = datetime.now()
 current_time = now.strftime("%d/%m/%y %H:%M")
 print("cotação gerada em ", current_time)
 
-MAX_WIDTH = 1280  # px
 FONT_HEIGHT = 60
 FONT = ImageFont.truetype("src/FreeSansBold.otf",FONT_HEIGHT)
 
-def make_image(img_path, msg1, msg2, output_path, pos1, pos2, color):
+def make_image(img_path, msg1, msg2, pos1, pos2, color, output_path):
     base = Image.open(img_path).convert('RGBA')
     txt = Image.new('RGBA', base.size, (255,255,255,0))
     d = ImageDraw.Draw(txt)
@@ -30,15 +31,14 @@ def get_dolar():
     cotacao = raw.text.split('\n')[1]
     return cotacao
 
-print(get_dolar())
-
-p2 = "ei carinha que mora logo ali"
-phrase = "me passa " + get_dolar().lower()
+frase1 = "ei carinha que mora logo ali"
+frase2 = "me passa " + get_dolar().lower()
 
 jerome = choice(['jerome1', 'jerome2', 'jerome3'])
+
 if jerome == 'jerome1':
-    make_image("src/jerome1.jpg", phrase, p2, "teste.png", (400, 600), (240, 30), 'yellow')
+    make_image("src/jerome1.jpg", frase1, frase2, (240, 30), (400, 600), 'yellow', "output.png")
 if jerome == "jerome2":
-    make_image("src/jerome2.jpg", phrase, p2, "teste.png", (300, 600), (200, 50), '#e3c520')
+    make_image("src/jerome2.jpg", frase1, frase2, (200, 50), (300, 600), '#e3c520', "output.png")
 if jerome == "jerome3":
-    make_image("src/jerome3.jpg", phrase, p2, "teste.png", (200, 550), (150, 50), '#f5d10c')
+    make_image("src/jerome3.jpg", frase1, frase2, (150, 50), (200, 550), '#f5d10c', "output.png")
